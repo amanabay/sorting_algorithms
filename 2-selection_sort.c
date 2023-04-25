7eeb91a5061a890b97a9aa3d57d3b50286dbd1f5
amanabay <amanuelabay22@gmail.com>
2023-04-25 02:44:14 +0000

    Betty fix

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 
 /**
- * bubble_sort - sorts an array of integers in ascending orderusing
+ * selection_sort - sorts an array of integers in ascending orderusing
  * the selection sort algorithm
  *
  * @array: array to be sorted
@@ -17,16 +17,15 @@ void selection_sort(int *array, size_t size)
 	if (array == NULL)
 		return;
 
-	for(i = 0; i < size - 1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		size_t last = size - i - 1;
 		int pos = 0;
 
-		for(j = 1; j <= last; j++)
-		{
-			if(array[j] > array[pos])
+		for (j = 1; j <= last; j++)
+			if (array[j] > array[pos])
 				pos = j;
-		}
+
 		temp = array[pos];
 		array[pos] = array[last];
 		array[last] = temp;
