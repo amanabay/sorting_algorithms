066e45e1801da9634204f79ddaa4441e3eef49e6
amanabay <amanuelabay22@gmail.com>
2023-04-25 13:49:34 +0000

    Quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,78 @@
+#include "sort.h"
+
+/**
+ * swap - swaps two integers
+ *
+ * @x: first integer
+ * @y: second integer
+ *
+ */
+
+void swap(int *x, int *y)
+{
+	int temp;
+	temp = *x;
+	*x = *y;
+	*y = temp;
+}
+
+/**
+ * lomuto_partition - partition an array using lomuto partition
+ * scheme
+ *
+ * @array: array to be partitioned
+ * @high: end of the array
+ * @low: starting comparison point
+ */
+
+int lomuto_partition(int array[], size_t size, int low, int high)
+{
+	int pivot = array[high], i = low - 1, j = low;
+ 
+	for (j = low; j <= high - 1; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			i++;
+			swap(&array[i], &array[j]);
+			print_array(array, size);
+		}
+	}
+	swap(&array[i + 1], &array[high]);
+	return (i + 1);
+}
+
+/**
+ * lomuto_sort - sorts the array rcursively
+ *
+ * @array: array to be sorted
+ * @high: end of the array
+ * @low: starting comparison point
+ *
+ */
+
+void lomuto_sort(int array[], size_t size, int low, int high)
+{
+	if (low < high)
+	{
+		int part_idx = lomuto_partition(array, size, low, high);
+
+		lomuto_sort(array, size, low, part_idx - 1);
+		lomuto_sort(array, size, part_idx + 1, high);
+    }
+}
+
+/**
+ * quick_sort - Sorts array using quick_sort algorithm
+ *
+ * @array: array to be sorted
+ * @size: size of the array
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	if (!array || size < 2)
+		return;
+
+	lomuto_sort(array, size, 0, size - 1);
+}
